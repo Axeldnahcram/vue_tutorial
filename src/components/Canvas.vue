0164a7516634dda62dc15b7eb3d72bb0339e2135
Axel MARCHAND <amarchand@Axels-MacBook-Pro.local>
2019-04-18 18:54:01 +0200

    AXEL -UPD- bunch of stuff

---
+++ src/components/Canvas.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="canvas">
+        <Pixel v-for="n in 30*30" :key="n" color="blue"/>
+    </div>
+</template>
+<script>
+import Pixel from './Pixel'
+export default {
+    name: 'Canvas',
+    components: {
+        Pixel
+    }
+}
+</script>
+<style scoped>
+.canvas {
+    display: flex;
+    flex-wrap: wrap;
+    max-width: 900px;
+}
+</style>
